93b84d910794cdf48c52746ebf85f54a27aeb0ec
Gerrit Beine <gerrit.beine@gmx.de>
2017-01-26 14:36:22 +0100

    Keep gitfs running ;-)

---
+++ libgit2.rb
@@ -0,0 +1,64 @@
+class Libgit2 < Formula
+  desc "C library of Git core methods that is re-entrant and linkable"
+  homepage "https://libgit2.github.com/"
+  url "https://github.com/libgit2/libgit2/archive/v0.24.5.tar.gz"
+  sha256 "f6135ee64b174f449c8857272352c11ca182af05a340237834cedcc9eb390cba"
+  head "https://github.com/libgit2/libgit2.git"
+
+  bottle do
+    sha256 "810f5055462205c867b65d6d2bb830f94ccdb001172245e9ced873c8febf53c7" => :sierra
+    sha256 "4f05d9aa455d1b6f6e8608d48acc7522804fd514bbfcb7e259a651e05b96e44f" => :el_capitan
+    sha256 "e837edf7cee23fa3ff9b04583dee57bba2496bcd062f8e48704767abae242255" => :yosemite
+  end
+
+  option :universal
+
+  depends_on "pkg-config" => :build
+  depends_on "cmake" => :build
+  depends_on "libssh2" => :recommended
+  depends_on "openssl" if MacOS.version <= :lion # Uses SecureTransport on >10.7
+
+  def install
+    args = std_cmake_args
+    args << "-DBUILD_EXAMPLES=YES"
+    args << "-DBUILD_CLAR=NO" # Don't build tests.
+    args << "-DUSE_SSH=NO" if build.without? "libssh2"
+
+    if build.universal?
+      ENV.universal_binary
+      args << "-DCMAKE_OSX_ARCHITECTURES=#{Hardware::CPU.universal_archs.as_cmake_arch_flags}"
+    end
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+      cd "examples" do
+        (pkgshare/"examples").install "add", "blame", "cat-file", "cgit2",
+                                      "describe", "diff", "for-each-ref",
+                                      "general", "init", "log", "remote",
+                                      "rev-list", "rev-parse", "showindex",
+                                      "status", "tag"
+      end
+    end
+  end
+
+  test do
+    (testpath/"test.c").write <<-EOS.undent
+      #include <git2.h>
+
+      int main(int argc, char *argv[]) {
+        int options = git_libgit2_features();
+        return 0;
+      }
+    EOS
+    libssh2 = Formula["libssh2"]
+    flags = %W[
+      -I#{include}
+      -I#{libssh2.opt_include}
+      -L#{lib}
+      -lgit2
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
